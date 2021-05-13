d324b3171e54f9c8f38ab78852a3711be91aabb7
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-13 20:16:18 -0300

    Seeding da base de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_product(name, description, price, img_url) VALUES ('Código Limpo', 'Boas práticas de programação', 70.98, '');
+INSERT INTO tb_category(name, product_id) VALUES ('Book', 1);
