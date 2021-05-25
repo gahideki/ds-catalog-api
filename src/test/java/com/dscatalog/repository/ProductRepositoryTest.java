30d169fb5500d1bfda933ab331a90fc93fab0b1a
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-24 21:31:05 -0300

    Testando outro cenário do delete

--- src/test/java/com/dscatalog/repository/ProductRepositoryTest.java
+++ src/test/java/com/dscatalog/repository/ProductRepositoryTest.java
@@ -4,9 +4,11 @@ import com.dscatalog.model.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
@@ -21,6 +23,14 @@ class ProductRepositoryTest {
         Optional<Product> product = productRepository.findById(1L);
 
         assertTrue(product.isEmpty());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+        long nonExistingId = 1000L;
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+           productRepository.deleteById(nonExistingId);
+        });
     }
 
 }
