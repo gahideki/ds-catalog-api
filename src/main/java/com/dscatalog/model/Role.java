108fafecd56a451598f34a01f9fd61415e552007
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-06-02 00:32:34 -0300

    ORM mapping e database seeding

--- src/main/java/com/dscatalog/model/Role.java
+++ src/main/java/com/dscatalog/model/Role.java
@@ -1,13 +1,23 @@
 package com.dscatalog.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_role")
 public class Role {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String authority;
 
 }
